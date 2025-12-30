650b08cf85ea4efbdab4fc9bee012d9096298c2b
vininiceto <viniciusniceto@gmail.com>
2025-12-30 10:03:05 -0300

    Adicionando a atualização de status entre ativo e inativo.

--- src/main/java/br/com/vininiceto/plataformacursos/controller/CursoController.java
+++ src/main/java/br/com/vininiceto/plataformacursos/controller/CursoController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -20,6 +21,11 @@ public class CursoController {
     @GetMapping("/{id}")
     public ResponseEntity<Optional<Curso>> findCursoById(@PathVariable String id) {
         return ResponseEntity.status(HttpStatus.OK).body(service.findById(id));
+    }
+
+    @GetMapping("/todos")
+    public ResponseEntity<Optional<List<Curso>>> findAllCursos(){
+        return ResponseEntity.status(HttpStatus.OK).body(Optional.ofNullable(service.findAllCurso()));
     }
 
     @GetMapping("/{name}/{category}")
@@ -36,6 +42,12 @@ public class CursoController {
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> deleteCurso(@PathVariable String id){
         return ResponseEntity.status(HttpStatus.OK).body(service.deleteCurso(id));
+    }
+
+    @PatchMapping("/status/{id}")
+    public ResponseEntity<Optional<Curso>> changeStatus(@PathVariable String id){
+        Curso cursoAtualizado = service.changeStatusCurso(id);
+        return ResponseEntity.status(HttpStatus.OK).body(Optional.ofNullable(cursoAtualizado));
     }
 
 
--- src/main/java/br/com/vininiceto/plataformacursos/service/CursoService.java
+++ src/main/java/br/com/vininiceto/plataformacursos/service/CursoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -21,19 +22,28 @@ public class CursoService {
         return repository.saveAndFlush(curso);
     }
 
-    public Optional<Curso> findById(String id){
+    public Optional<Curso> findById(String id) {
         return Optional.of(repository.findById(Long.parseLong(id)).orElseThrow(() -> new CursoNotFound("Id invalid!")));
     }
 
-    public Optional<Curso> findCursoByNameAndCategory(String name, String category){
+    public List<Curso> findAllCurso(){
+        return repository.findAll();
+    }
+
+    public Optional<Curso> findCursoByNameAndCategory(String name, String category) {
         return Optional.of(Optional.of(repository.findCursoByNameAndCategory(name, category)).orElseThrow(() -> new CursoNotFound("Name or category invalid!")));
     }
 
-    public String deleteCurso(String id){
+    public String deleteCurso(String id) {
         repository.deleteById(Long.parseLong(id));
         return "Course deleted as requested.";
     }
 
+    public Curso changeStatusCurso(String id) {
+        Curso curso = repository.findById(Long.parseLong(id)).orElseThrow(() -> new CursoNotFound("Id Invalid!"));
+        curso.setActive(!curso.getActive());
+        return repository.saveAndFlush(curso);
+    }
 
 
 }
