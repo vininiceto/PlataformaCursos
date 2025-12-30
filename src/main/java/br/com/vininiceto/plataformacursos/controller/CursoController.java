58ab756aeb17a9702919de8c21f038546bfa679c
vininiceto <viniciusniceto@gmail.com>
2025-12-30 10:53:49 -0300

    Adicionado metodo Put para realizar atualização de nome ou categoria

--- src/main/java/br/com/vininiceto/plataformacursos/controller/CursoController.java
+++ src/main/java/br/com/vininiceto/plataformacursos/controller/CursoController.java
@@ -1,5 +1,6 @@
 package br.com.vininiceto.plataformacursos.controller;
 
+import br.com.vininiceto.plataformacursos.dto.CursosPutDto;
 import br.com.vininiceto.plataformacursos.model.Curso;
 import br.com.vininiceto.plataformacursos.service.CursoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,12 +20,12 @@ public class CursoController {
 
 
     @GetMapping("/{id}")
-    public ResponseEntity<Optional<Curso>> findCursoById(@PathVariable String id) {
+    public ResponseEntity<Optional<Curso>> findCursoById(@PathVariable Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(service.findById(id));
     }
 
     @GetMapping("/todos")
-    public ResponseEntity<Optional<List<Curso>>> findAllCursos(){
+    public ResponseEntity<Optional<List<Curso>>> findAllCursos() {
         return ResponseEntity.status(HttpStatus.OK).body(Optional.ofNullable(service.findAllCurso()));
     }
 
@@ -40,15 +41,19 @@ public class CursoController {
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<String> deleteCurso(@PathVariable String id){
+    public ResponseEntity<String> deleteCurso(@PathVariable Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(service.deleteCurso(id));
     }
 
     @PatchMapping("/status/{id}")
-    public ResponseEntity<Optional<Curso>> changeStatus(@PathVariable String id){
+    public ResponseEntity<Optional<Curso>> changeStatus(@PathVariable Long id) {
         Curso cursoAtualizado = service.changeStatusCurso(id);
         return ResponseEntity.status(HttpStatus.OK).body(Optional.ofNullable(cursoAtualizado));
     }
 
-
+    @PutMapping("/change/{id}")
+    public ResponseEntity<Optional<Curso>> changeNameOrCategory(@PathVariable Long id, @RequestBody CursosPutDto cursoDto){
+        Curso cursoAtualizado = service.changeNameOrCategory(id,cursoDto);
+        return ResponseEntity.status(HttpStatus.OK).body(Optional.ofNullable(cursoAtualizado));
+    }
 }
--- src/main/java/br/com/vininiceto/plataformacursos/service/CursoService.java
+++ src/main/java/br/com/vininiceto/plataformacursos/service/CursoService.java
@@ -1,5 +1,6 @@
 package br.com.vininiceto.plataformacursos.service;
 
+import br.com.vininiceto.plataformacursos.dto.CursosPutDto;
 import br.com.vininiceto.plataformacursos.exceptions.CursoNotFound;
 import br.com.vininiceto.plataformacursos.model.Curso;
 import br.com.vininiceto.plataformacursos.repository.CursoRepository;
@@ -22,11 +23,11 @@ public class CursoService {
         return repository.saveAndFlush(curso);
     }
 
-    public Optional<Curso> findById(String id) {
-        return Optional.of(repository.findById(Long.parseLong(id)).orElseThrow(() -> new CursoNotFound("Id invalid!")));
+    public Optional<Curso> findById(Long id) {
+        return Optional.of(repository.findById(id).orElseThrow(() -> new CursoNotFound("Id invalid!")));
     }
 
-    public List<Curso> findAllCurso(){
+    public List<Curso> findAllCurso() {
         return repository.findAll();
     }
 
@@ -34,16 +35,26 @@ public class CursoService {
         return Optional.of(Optional.of(repository.findCursoByNameAndCategory(name, category)).orElseThrow(() -> new CursoNotFound("Name or category invalid!")));
     }
 
-    public String deleteCurso(String id) {
-        repository.deleteById(Long.parseLong(id));
+    public String deleteCurso(Long id) {
+        repository.deleteById(id);
         return "Course deleted as requested.";
     }
 
-    public Curso changeStatusCurso(String id) {
-        Curso curso = repository.findById(Long.parseLong(id)).orElseThrow(() -> new CursoNotFound("Id Invalid!"));
+    public Curso changeStatusCurso(Long id) {
+        Curso curso = repository.findById(id).orElseThrow(() -> new CursoNotFound("Id Invalid!"));
         curso.setActive(!curso.getActive());
         return repository.saveAndFlush(curso);
     }
 
+    public Curso changeNameOrCategory(Long id, CursosPutDto cursoDto) {
+        Curso curso = repository.findById(id).orElseThrow(() -> new CursoNotFound("Id Invalid!"));
+        if (cursoDto.getName() != null) {
+            curso.setName(cursoDto.getName());
+        }
+        if (cursoDto.getCategory() != null) {
+            curso.setCategory(cursoDto.getCategory());
+        }
+        return repository.saveAndFlush(curso);
+    }
 
 }
